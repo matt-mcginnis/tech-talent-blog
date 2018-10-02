4de345c000d587758207641e84c647ebf3fa64e5
Matt McGinnis <mcginnisma90@gmail.com>
2018-10-02 10:48:56 -0400

    Added some styling.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   resources :comments
   resources :posts
-  get 'index' => 'practice#index'
-  post 'index' => 'practice#index'
-  get 'about' => 'practice#about'
-  root 'practice#index'
+
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
